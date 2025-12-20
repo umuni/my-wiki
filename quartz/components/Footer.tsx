aa50e1190280cde1936aba6c4b9d04ef1f53e3c5
jitemer <1125923836@qq.com>
2025-12-20 19:18:15 +0800

    fix: 修复页脚文字重叠错误，优化版权链接

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,21 +1,18 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
-import { version } from "../../package.json"
-import { i18n } from "../i18n"
 
 interface Options {
   links: Record<string, string>
 }
 
 export default ((opts?: Options) => {
-  const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
+  const Footer: QuartzComponent = ({ displayClass }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{"powered by strveen"}
-          <a href="https://aitest.eu.cc">strveen v{version}</a> © {year}
+  Powered by <a href="https://aitest.eu.cc">Strveen</a> © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
